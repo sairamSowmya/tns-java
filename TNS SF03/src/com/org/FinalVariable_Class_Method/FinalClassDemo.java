5a8019fff014338b1494ac384ab5949569dcfc30
SOWMYA <SOWMYA@sowm>
2024-06-16 21:32:49 +0530

    Commit 5

--- TNS SF03/src/com/org/FinalVariable_Class_Method/FinalClassDemo.java
+++ TNS SF03/src/com/org/FinalVariable_Class_Method/FinalClassDemo.java
@@ -5,14 +5,13 @@ package com.org.FinalVariable_Class_Method;
 	void show() {
 		System.out.println("Final class cannot be inherited");
 	}
-}
+
 
 //can't create child classes from Final class, Ex: String, Wrapper Classes, System, Scanner, Number are Final classes
 //class FinalChildClass extends FinalClass {
 //}
 
-@Override
- public class FinalClassDemo {
+ //public class FinalClassDemo {
 	public static void main(String[] args) {
 		// Create the object of final class
 		FinalClass f1 = new FinalClass(); // Call show() method using object reference variable ab.
