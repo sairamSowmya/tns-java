bdb3a2e15f2e524c8b63f9800a65edd89b8c7955
SOWMYA <SOWMYA@sowm>
2024-06-16 21:26:14 +0530

    commit 3

---
+++ TNS SF03/src/com/org/ImplementingInterface/BankImpl.java
@@ -0,0 +1,17 @@
+package com.org.ImplementingInterface;
+
+public class BankImpl implements Bank {
+
+	@Override
+	public void deposit(Account account, double amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void withdraw(Account account, double amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
