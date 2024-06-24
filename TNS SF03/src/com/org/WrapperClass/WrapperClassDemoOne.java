6a0cf4fb3a4f463f8695a045c5bd6dde4e607f31
SOWMYA <SOWMYA@sowm>
2024-06-24 20:45:27 +0530

    Wrapper

---
+++ TNS SF03/src/com/org/WrapperClass/WrapperClassDemoOne.java
@@ -0,0 +1,14 @@
+package com.org.WrapperClass;
+
+public class WrapperClassDemoOne {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+
+		int a = Integer.parseInt("0");
+		int b = Integer.parseInt("1");
+		int c = a + b;
+		System.out.println(c);
+	}
+
+
+}
