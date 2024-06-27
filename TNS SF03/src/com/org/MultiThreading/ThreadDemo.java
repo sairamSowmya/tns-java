fe0a00f920d8e8eac0c17578849b7fa9d4f96cb2
SOWMYA <SOWMYA@sowm>
2024-06-27 20:15:52 +0530

    MultiThreading

---
+++ TNS SF03/src/com/org/MultiThreading/ThreadDemo.java
@@ -0,0 +1,18 @@
+//Executor class for ChildThread
+package com.org.MultiThreading;
+
+public class ThreadDemo {
+	public static void main(String[] args) {
+		ChildThread threadOne = new ChildThread(5, "First");
+		ChildThread threadTwo = new ChildThread(10, "Second");
+
+		threadOne.start();
+		//threadOne.start(); // throws IllegalStateException
+		// threadOne.run(); // single threaded application 
+		threadTwo.start();
+
+		System.out.println("-----------------------End of Main--------------------------");
+	}
+
+
+}
